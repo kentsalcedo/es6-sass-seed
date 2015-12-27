baac57177185d5985728d583cc43c1b2bdbd9c35
Kent Salcedo <kentsalcedo@Kents-MacBook-Pro.local>
2015-12-26 15:01:48 -1000

    update gulpfile.js to add sass task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
 var gulp = require('gulp');
-var babel = require('gulp-babel');
+var babel = require('gulp-babel');var sass = require('gulp-sass');
 
+// sass tasks
+gulp.task('sass', function () {
+  gulp.src('scss/style.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('public/css'));
+});
+
+// es6 tasks
 gulp.task('default', function() {
   return gulp.src('src/test.js')
     .pipe(babel({
